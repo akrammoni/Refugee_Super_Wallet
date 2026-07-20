3a0854f778ecba16d432dc545b3523663687d8ae
Akram Moni <akrammoni6@gmail.com>
2026-07-20 09:07:27 +0000

    Configure app port for Render

--- app/main.py
+++ app/main.py
@@ -56,4 +56,4 @@ def create_transaction():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=int(__import__("os").environ.get("PORT", 8080)), debug=False)
